ebea70d2fefe13bf8060d25d0ae92a5dc7f6c6a8
mindnervestech <jagbir.paul@gmail.com>
2014-08-14 01:28:46 +0530

    bug

--- app/com/pharmakhana/web/controllers/DefinedLocationsController.java
+++ app/com/pharmakhana/web/controllers/DefinedLocationsController.java
@@ -8,28 +8,28 @@ import play.libs.Json;
 import play.mvc.Controller;
 import play.mvc.Result;
 
-import com.pharmakhana.models.Country;
-import com.pharmakhana.models.Language;
 import com.pharmakhana.models.PreDefinedLocations;
 import com.pharmakhana.util.CommonUtils;
+import com.pharmakhana.web.resources.DefinedLocationResource;
 
 public class DefinedLocationsController extends Controller {
 	@Transactional(readOnly=true)
 	public static Result getPreDefinedLocations(final String langCode, final String countryCode) {
 		System.out.println("getPreDefinedLocations :: langCode: " + langCode + " countryCode: " + countryCode);
-/*    	
-		PreDefinedLocations location = new PreDefinedLocations();
-		Language language = new Language();
-		language.setLanguageCode(langCode);
-		Country country = new Country();
-		country.setCountryCode(countryCode);
-*/
 		List<PreDefinedLocations> definedLocations = PreDefinedLocations.findLocationsByCountry(langCode, countryCode);
+		List<DefinedLocationResource> locationResources = new ArrayList<DefinedLocationResource>();
 
 		if (definedLocations == null || definedLocations.isEmpty()) {
-			definedLocations = new ArrayList<>();
+		definedLocations = new ArrayList<>();
+		} else {
+		for (PreDefinedLocations location: definedLocations) {
+		DefinedLocationResource res = new DefinedLocationResource();
+		res.setLocationName(location.getLocationName());
+		res.setLocationId(location.getLocId());
+		locationResources.add(res);
 		}
-		
-        return ok(Json.toJson(CommonUtils.prepareResponse(definedLocations)));
-    }
+		}
+
+		return ok(Json.toJson(CommonUtils.prepareResponse(locationResources)));
+		}
 }
